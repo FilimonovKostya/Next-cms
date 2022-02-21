2dd8322e3fe16855bc4f6ae41abee74c4784c367
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-21 14:04:53 +0300

    integration admin

---
+++ contentful/index.ts
@@ -0,0 +1,8 @@
+import {createClient} from "contentful";
+
+const client = createClient({
+    space: 'gg9vrj4o28f1',
+    accessToken: 'p7rq8dFINFqNj1bIrYv2p4pOyeT-p4SZKyr9AJjRuLY'
+})
+
+export default client
