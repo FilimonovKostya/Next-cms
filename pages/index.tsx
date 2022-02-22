21327f7a665addee8cd9fdce02fea1a69a8645ab
FilimonovKostya <konstantinfilimonou@gmail.com>
2022-02-22 14:05:21 +0300

    added post

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,11 +2,14 @@ import type {GetStaticProps} from 'next'
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 import client from "../contentful/index";
-import {IMainPage, IMainPageFields} from "../contentful-types";
+import {IArcticle, IArcticleFields, IMainPage, IMainPageFields} from "../contentful-types";
 import {documentToReactComponents} from '@contentful/rich-text-react-renderer';
+import {Button, Card, CardText, CardTitle, Col, Container, Row} from "reactstrap";
+import Link from 'next/link';
 
 
-const Home = ({home}: { home: IMainPage }) => {
+const Home = ({home, arcticle}: { home: IMainPage, arcticle: IArcticle[] }) => {
+    console.log(arcticle)
     return (
         <div className={styles.container}>
             <Head>
@@ -16,9 +19,26 @@ const Home = ({home}: { home: IMainPage }) => {
                 <div className={'text-center p-5 text-white'} style={{
                     background: `url("http:${home.fields.image?.fields.file.url}") no-repeat center / cover`,
                     minHeight: 300
-                }}/>
-                <h1 className={'mt-5'}>{home.fields.heading}</h1>
-                <div className={'mb-5'}>{documentToReactComponents(home.fields.underHeading!)}</div>
+                }}>
+                    <h1 className={'mt-5'}>{home.fields.heading}</h1>
+                    <div className={'mb-5'}>{documentToReactComponents(home.fields.underHeading!)}</div>
+                </div>
+
+                <Container className={'pt-5'} >
+                    <Row>
+                        {arcticle.map(el => (
+                            <Col sm={4} key={el.fields.slug}>
+                                <Card body>
+                                    <CardTitle tag={'h5'}>{el.fields.title}</CardTitle>
+                                    <CardText>{el.fields.description}</CardText>
+                                    <Link href={`/arcticles/${el.fields.slug}`}><Button>{el.fields.action}</Button></Link>
+                                </Card>
+                            </Col>
+                        ))}
+
+                    </Row>
+                </Container>
+
             </main>
         </div>
     )
@@ -30,12 +50,16 @@ export const getStaticProps: GetStaticProps = async () => {
         limit: 1
     })
 
+    const articleEntries = await client.getEntries<IArcticleFields>({
+        content_type: 'arcticle',
+        select: 'fields.title,fields.slug,fields.description,fields.action'
+    })
 
 
     const [homePage] = home.items
 
     return {
-        props: {title: 'My Blog', home: homePage}
+        props: {title: 'My Blog', home: homePage, arcticle: articleEntries.items}
     }
 }
 
